fd03dfc4676efa05153f0b32bae53761632e869d
Lucas Romero Di Benedetto <lucasromerodb@gmail.com>
2019-11-07 23:51:18 -0300

    custom environment variables configuration

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ app.use(helmet()); // Helps secure your apps by setting various HTTP headers.
 
 console.log("App name:", config.get("name"));
 console.log("Mail server:", config.get("mail.host"));
+console.log("Mail password:", config.get("mail.password"));
 
 if (app.get("env") === "development") {
   app.use(morgan("tiny")); // HTTP request logger.
